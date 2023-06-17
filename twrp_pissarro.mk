0a95e3d498ac4231bcbba76aedf591613c11b796
HuaLiMao-AQ <hualimao_aq@163.com>
2023-06-17 18:19:13 +0800

    pissarro: Initial deviceTree

---
+++ twrp_pissarro.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from pissarro device
+$(call inherit-product, device/xiaomi/pissarro/device.mk)
+
+PRODUCT_DEVICE := pissarro
+PRODUCT_NAME := twrp_pissarro
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := pissarro
+PRODUCT_MANUFACTURER := xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
