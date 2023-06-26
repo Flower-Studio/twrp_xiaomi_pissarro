4cf64039217ed586f09b03cdafc998942018dd8b
HuaLiMao-AQ <hualimao_aq@163.com>
2023-06-26 14:00:06 +0800

    pissarro: Support vab

--- device.mk
+++ device.mk
@@ -7,8 +7,24 @@
 
 LOCAL_PATH := device/xiaomi/pissarro
 # A/B
+AB_OTA_PARTITIONS += \
+    boot \
+    dtbo \
+    lk \
+    preloader \
+    product \
+    system \
+    vbmeta \
+    vbmeta_system \
+    vbmeta_vendor \
+    vendor \
+
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
+
+# V A/B
+ENABLE_VIRTUAL_AB := true
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
